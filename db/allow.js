bdc1d32d452e9d36ef4802122e1e72be793d267c
제리 <jaryapp@gmail.com>
2020-11-18 23:43:51 +0900

    [#26] feat: pause api

--- db/allow.js
+++ db/allow.js
@@ -16,6 +16,12 @@ async function getAllowSiteList(token) {
   return rows;
 }
 
+async function getAllowOnSiteList(token) {
+  const query = `SELECT site.* FROM push_allow INNER JOIN site ON push_allow.site_id = site.id WHERE fcm_token = '${token}' `;
+  let [rows, fields] = await connection.promise().query(query);
+  return rows;
+}
+
 async function setAllowSite({ fcm_token, site_id }) {
   const query = mysql.format("INSERT INTO push_allow SET ?", {
     fcm_token,
@@ -34,8 +40,35 @@ async function unsetAllowSite({ fcm_token, site_id }) {
   return rows;
 }
 
+async function getPause({ fcm_token }) {
+  const query = mysql.format("SELECT * FROM push_pause WHERE fcm_token = ?", [
+    fcm_token,
+  ]);
+  let [rows, fields] = await connection.promise().query(query);
+  return rows;
+}
+
+async function setPause({ fcm_token }) {
+  const query = mysql.format("INSERT INTO push_pause SET ?", {
+    fcm_token,
+  });
+  let [rows, fields] = await connection.promise().query(query);
+  return rows;
+}
+
+async function unsetPause({ fcm_token }) {
+  const query = mysql.format("DELETE FROM push_pause WHERE fcm_token = ?", [
+    fcm_token,
+  ]);
+  let [rows, fields] = await connection.promise().query(query);
+  return rows;
+}
 module.exports = {
   getAllowSiteList,
+  getAllowOnSiteList,
   setAllowSite,
   unsetAllowSite,
+  getPause,
+  setPause,
+  unsetPause,
 };
